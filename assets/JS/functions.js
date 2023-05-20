4e7f711f8b20d2b5eb260641e4f28c48d43966f5
AhmedOsman101 <trollingosman101@outlook.com>
2023-05-20 20:24:57 +0300

    moved changes from test to main files

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -1,109 +1,74 @@
-/* const resetFunc = () => {
-    board.innerHTML = "";
-    createBoard();
-    currentTurn = "x";
-    circleTurn = false;
-    isDraw = false;
-    isWon = false;
-    xLocations = [];
-    circleLocations = [];
-    displayTurns();
-    winningMessage.style.display = "none";
-    reset.style.display = "block";
-};
-*/
-
-/*
 const displayTurns = () => {
-    squares = document.querySelectorAll(".square");
-    playerTurn.innerHTML = "";
+    playerTurn.innerHTML = !circleTurn
+        ? `Player ${XElement}'s turn`
+        : `Player ${OElement}'s turn`;
+};
+
+const swapTurns = () => {
+    circleTurn = !circleTurn;
+};
+
+const clicking = (e) => {
+    const currentSquare = e.target;
+    currentSquare.innerHTML = !circleTurn ? XElement : OElement;
     if (!circleTurn) {
-        currentTurn = "x";
-        circleTurn = true;
-        playerTurn.innerHTML = `Player ${XElement}'s turn`;
+        xLocations.push(Number(e.target.getAttribute("data-current")));
     } else {
-        currentTurn = "o";
-        circleTurn = false;
-        playerTurn.innerHTML = `Player ${OElement}'s turn`;
+        circleLocations.push(Number(e.target.getAttribute("data-current")));
     }
+    swapTurns();
+    displayTurns();
+    checkWinner();
 };
-*/
 
-/*
-const createBoard = () => {
-    for (let i = 0; i < 9; i++) {
-        const squareElement = `<div class="square" data-current="${i}"></div>`;
-        board.innerHTML += squareElement;
-    }
-    squares = document.querySelectorAll(".square");
-    squares.forEach((element) => {
-        element.addEventListener(
-            "click",
-            () => {
-                element.innerHTML = currentTurn == "x" ? XElement : OElement;
-                if (circleTurn) {
-                    xLocations.push(
-                        Number(element.getAttribute("data-current"))
-                    );
-                } else {
-                    circleLocations.push(
-                        Number(element.getAttribute("data-current"))
-                    );
-                }
-                displayTurns();
-                checkWinner();
-            },
-            { once: true }
-        );
+const initGame = () => {
+    displayTurns();
+    squares.forEach((square) => {
+        square.innerHTML = "";
+        square.addEventListener("click", clicking, { once: true });
     });
+    reset.addEventListener("click", resetFunc);
 };
-*/
 
-/*
 const checkWinner = () => {
     for (let i = 0; i < winningCombos.length; i++) {
         const combo = winningCombos[i];
         if (combo.every((index) => xLocations.includes(index))) {
             playerTurn.innerHTML = `Player ${XElement} won!`;
-            disableSquares(XElement);
-            isDraw = false;
-            isWon = true;
-            break;
+            showEndMessage(XElement);
+            return;
         } else if (combo.every((index) => circleLocations.includes(index))) {
             playerTurn.innerHTML = `Player ${OElement} won!`;
-            disableSquares(OElement);
-            isDraw = false;
-            isWon = true;
-            break;
+            showEndMessage(OElement);
+            return;
         }
     }
-    if (
-        xLocations.length + circleLocations.length == squares.length &&
-        isWon == false
-    ) {
+    checkDraw();
+};
+
+const checkDraw = () => {
+    if (xLocations.length + circleLocations.length == 9) {
         isDraw = true;
-        disableSquares("It's a draw!");
-    } else {
-        console.log("thereIsNoWinner");
+        playerTurn.innerHTML = `It's a draw!`;
+        showEndMessage("It's a draw!");
     }
 };
- */
 
-/*
-const disableSquares = (winner) => {
+const showEndMessage = (winner) => {
     const winningMessage = document.getElementById("winningMessage");
-    if (isDraw == true) {
-        winningMessage.innerHTML = `${winner}
-        ${newGame}`;
-    } else {
-        winningMessage.innerHTML = `
-        Player ${winner} won!
-        ${newGame}`;
-    }
-    document.querySelector(".reset").addEventListener("click", resetFunc);
+    winningMessage.innerHTML =
+        isDraw == true ? `${winner}` : `Player${winner} won!`;
     winningMessage.style.display = "grid";
-    reset.style.display = "none";
+    const clone = reset.cloneNode(true);
+    clone.addEventListener("click", resetFunc);
+    winningMessage.appendChild(clone);
+};
+
+const resetFunc = () => {
+    circleTurn = false;
+    isDraw = false;
     xLocations = [];
     circleLocations = [];
+    initGame();
+    winningMessage.style.display = "none";
 };
-*/
